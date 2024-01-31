16ce39711fd073fa24dc8ab3c111fb4330116eaa
Flokhs <158329705+Flokhs@users.noreply.github.com>
2024-01-31 20:32:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,26 +5,19 @@ function showMessage(response) {
     const maxWidth = window.innerWidth - noButton.offsetWidth;
     const maxHeight = window.innerHeight - noButton.offsetHeight;
 
-    // Set button position to absolute
     noButton.style.position = "absolute";
 
-    // Change image source to "gun.gif"
-    document.getElementsByClassName("image")[0].src = "images/gun.gif";
+  
+    document.getElementsByClassName("image")[0].src = "imgs/sroulik.jpg";
 
-    // Generate random coordinates within the visible container
     const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
     const randomY = Math.max(0, Math.floor(Math.random() * maxHeight));
-
-    // Apply new coordinates to the button
+  
     noButton.style.left = randomX + "px";
     noButton.style.top = randomY + "px";
-
-    // Update text content and hide name message
     document.getElementById("question").textContent =
       "The no button is just for visuals";
-    document.getElementById("name").style.display = "none";
-
-    // Optional: You can also add a timeout to reset the position after a few seconds
+    document.getElementById("name").style.display = "none"; 
   }
 
   if (response === "Yes") {
@@ -32,14 +25,14 @@ function showMessage(response) {
     document.getElementById("name").remove();
     document.getElementById("no-button").remove();
 
-    // Update text content, show message, and change image source to "dance.gif"
+
     const yesMessage = document.getElementById("question");
     yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "images/emeis.jpg";
 
-    // Remove yes button
+
     document.getElementById("yesButton").remove();
   }
 }
